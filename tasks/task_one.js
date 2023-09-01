b9086855b56183d0a9b5ea5ca0768f83a8d5587a
Chidubem <nwekedubemph@gmail.com>
2023-09-01 03:32:36 -0700

    first message

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,2 +1,57 @@
+let messages = [];
 
-// Write something
+function sendMessage(senderId) {
+  const message = document.getElementById(senderId).value;
+  messages.push({
+    sender: senderId,
+    message: message
+  });
+
+  document.getElementById(senderId).value = "";
+
+  updateMessages();
+}
+
+
+function updateMessages() {
+  let messagesElement = document.getElementById("messages");
+  messagesElement.innerHTML = "";
+
+  for (let message of messages) {
+    let liElement = document.createElement("li");
+    liElement.textContent = message.sender + ": " + message.message;
+    messagesElement.appendChild(liElement);
+  }
+}
+
+document.getElementById("send1").addEventListener("click", function() {
+  sendMessage("sender1");
+});
+document.getElementById("send2").addEventListener("click", function() {
+  sendMessage("sender2");
+});
+
+messages.push({
+  sender: "Sender 1",
+  message: "Hello, world!"
+});
+messages.push({
+  sender: "Sender 2",
+  message: "Hi!"
+});
+
+function saveMessages() {
+  localStorage.setItem("messages", JSON.stringify(messages));
+}
+
+function loadMessages() {
+  let messagesString = localStorage.getItem("messages");
+  if (messagesString) {
+    messages = JSON.parse(messagesString);
+  }
+}
+
+
+window.addEventListener("beforeunload", saveMessages);
+
+loadMessages();
